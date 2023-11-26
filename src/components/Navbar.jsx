54fc49cad8e77249950c971934aa0e2b9d89a1d8
Phill <phillicia.mitchell21@gmail.com>
2023-11-26 18:55:59 -0500

    added dummy information and styled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
-    return (
-        <div>Navbar</div>
+import { Logo, UserTray } from "./index";
+
+const Navbar = () =>
+    (
+        <Stack 
+        direction='row'
+        alignItems='center'
+        p={3}
+        sx={{ 
+            position:'sticky',
+            background:'transparent',
+            top: 0,
+            justifyContent:'space-between'
+        }}>
+            <Link to='/' style={{ display:'flex', alignItems:'center' }}>
+                <Logo />
+            </Link>
+            <UserTray />
+        </Stack>
     )
-}
 
 export default Navbar
