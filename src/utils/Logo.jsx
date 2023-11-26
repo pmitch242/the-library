c5a682d426a03470e6a50d1dba54e20ed00a082b
Phill <phillicia.mitchell21@gmail.com>
2023-11-26 18:59:11 -0500

    added utiles folder with jsx logo

---
+++ src/utils/Logo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { Typography } from '@mui/material';
+
+const Logo = () => (
+        <Typography 
+        id='logo'
+        variant='h6'
+        sx={{ fontWeight: 'bold',
+        color: '#0a2800',
+        letterSpacing: 1.5
+        // fontFamily: 'Comfortaa',
+    }}
+        >THE LIBRARY</Typography>
+    )
+
+export default Logo
