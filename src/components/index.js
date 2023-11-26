e917b45e9ccc9e8672da7b8fa345708c72551dfd
Phill <phillicia.mitchell21@gmail.com>
2023-11-25 22:34:31 -0500

    added components folder

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Navbar } from './Navbar';
+export {default as BookDetail } from './BookDetail';
+export {default as Feed } from './Feed';
+export {default as Favorites } from './Favorites';
+export {default as SearchFeed } from './SearchFeed';
