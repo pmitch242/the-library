6706dfbbaf9a001feb32e1ab0c65a5dd176e2398
Phill <phillicia.mitchell21@gmail.com>
2023-11-25 22:35:54 -0500

    added dependencies and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-function App() {
-  return (
-    <div className="App">
-        <h1>Hey there Hannah!</h1>
-    </div>
-  );
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { Navbar, Feed, BookDetail, SearchFeed, Favorites } from './components';
+
+const App = () => 
+  <BrowserRouter>
+    <Box>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Feed />}/>
+        <Route path='/book/:id' element={<BookDetail />}/>
+        <Route path='/search/:searchTerm' element={<SearchFeed />}/>
+        <Route path='/favorites' element={<Favorites />}/>
+      </Routes>
+    </Box>
+  </BrowserRouter>
 
 export default App;
