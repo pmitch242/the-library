f1055156e3dbc6d857d9262a5d4f579236a433fc
Phill <phillicia.mitchell21@gmail.com>
2024-01-06 20:07:43 -0500

    added main menu buttons

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,15 +4,30 @@ import { menu, settingMenu } from "../utils/constants";
 
 const SideBar = () => (
     <Stack
-    direction= 'row'
-    sx={{
-        overflowY: 'auto',
-        height: 
-        {sx:'auto', md:'95%'},
-        flexDirection:'column'
-    }}
-        >
-        SideBar
+        direction='row'
+        sx={{
+            overflowY: 'auto',
+            height:
+                { sx: 'auto', md: '95%' },
+            flexDirection: 'column'
+        }}>
+        {menu.map((menu) => (
+            <button
+                className="menu-btn"
+                key={menu.name}
+            >
+                <span>{menu.icon}</span>
+                <span>{menu.name}</span>
+            </button>
+        ))}
+        {settingMenu.map((settingMenu) => (
+            <button
+                className="menu-btn"
+            >
+                <span>{settingMenu.icon}</span>
+                <span>{settingMenu.name}</span>
+            </button>
+        ))}
     </Stack>
 )
 
